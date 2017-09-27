02deddedb285455cd4d25ce62ebf947bc0ecbf61
Oier Saizar <oierlas@gmail.com>
2017-09-27 22:09:42 +0200

    clase de musica creada, 3 canciones base

--- js/Defines.js
+++ js/Defines.js
@@ -4,6 +4,9 @@ FRUTAS = [ ];
  /* Variable de cuchilllo */
 CUCHILLO = [ ];
 
+/* Variable de musica */
+MUSICA = [ ];
+
 /* Dimentsioak */
 CANVAS_WIDTH = 720;
 CANVAS_HEIGHT = 480;
@@ -18,7 +21,7 @@ G = 9.8;
 VY_KTE = 0.5;
 
 /* Zenbatero kalkulatzen den posizioa */
-OFFSET = 0.07; 
+OFFSET = 0.07;
 
 /* vy parametroak frutak pantaila barruan ager daitezen */
 VY_MAX = -95

b3f49bd5c55f6850224899e1874d2692e0508c08
Sampru <sampru_96@hotmail.com>
2017-09-28 16:32:08 +0200

    a beh si funsiona er hook

--- js/Gravity.js
+++ js/Gravity.js
@@ -1,7 +1,7 @@
 BUFFER = []
 
 function makeFall(posX0, vX, t, posY0, vY0) {
-  /* x = x0 + (vx * t) */
+		  /* x = x0 + (vx * t) */
   var x = posX0 + (vX * t);
   /* y = y0 + (vy0 * t) + (1/2 * a * t^2) */
   var y = posY0 + (vY0 * t) + (VY_KTE * G * t * t);
